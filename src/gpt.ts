e39bfbf235930c37956c0c521121e7b48772b614
Rongxin Liu <rongxinliu.dev@gmail.com>
2023-03-26 10:31:37 +0800

    load api key from settings

--- src/gpt.ts
+++ src/gpt.ts
@@ -1,19 +1,23 @@
+/* eslint-disable @typescript-eslint/naming-convention */
+
 const axios = require('axios');
 import * as vscode from 'vscode';
 
+const API_KEY = vscode.workspace.getConfiguration('copilot50').get('apikey');
 const openai = axios.create({
     baseURL: 'https://api.openai.com/v1',
     headers: {
         'Content-Type': 'application/json',
-        'Authorization': `Bearer OPENAI_API_KEY`
-    }});
+        'Authorization': `Bearer ${API_KEY}`
+    }
+});
 
 export async function processPrompt(codeSnippet: String) {
     vscode.window.showInformationMessage("Analyzing code...");
     let response: String = '';
     await openai.post('/chat/completions', {
         'model': 'gpt-3.5-turbo',
-        'messages': [{role: 'user', content: `Please explain the following code snippet: ${codeSnippet}`}],
+        'messages': [{ role: 'user', content: `Please explain the following code snippet: ${codeSnippet}` }],
     }).then((res: any) => {
         response = res.data.choices[0]['message']['content'];
     }).catch((err: any) => {
